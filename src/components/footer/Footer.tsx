2016efd9488daecdf130c90d400e949e82ba2d80
SACHSENSPIELT <sachsenspielt3@gmail.com>
2022-10-31 14:01:03 +0100

    Responsive Footer (#17)
    * fix: footer now mobile friendly; added imprint and privacy button
    * fix: added missing redirects to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,38 +1,56 @@
 import Image from "next/image";
 import SocialIcon from "./SocialIcon";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="flex w-full flex-col items-center gap-8">
       <div className="flex w-5/6 items-center justify-between">
         <section className="flex items-center gap-8">
-          <Image
-            src="/img/netzwerk.png"
-            alt="CubyxNetwork Logo"
-            width="256px"
-            height="256px"
-          />
+          <div className="hidden lg:block">
+            <Image
+              src="/img/netzwerk.png"
+              alt="CubyxNetwork Logo"
+              width="256px"
+              height="256px"
+            />
+          </div>
+
           <div>
             <h2 className="text-4xl font-extrabold">Cubyx Network</h2>
             <span>Dein demokratisches Bauteam aus Deutschland!</span>
             <div className="flex gap-2">
-              <SocialIcon icon={"discord-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"twitch-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"twitter-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"youtube-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"github-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"reddit-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"mastodon-fill"} link={"https://discord.gg/"} />
-              <SocialIcon icon={"earth-fill"} link={"https://discord.gg/"} />
+              <SocialIcon icon={"discord-fill"} link={"/r/discord"} />
+              <SocialIcon icon={"twitch-fill"} link={"/r/twitch"} />
+              <SocialIcon icon={"twitter-fill"} link={"/r/twitter"} />
+              <SocialIcon icon={"youtube-fill"} link={"/r/youtube"} />
+              <SocialIcon icon={"reddit-fill"} link={"/r/reddit"} />
+              <SocialIcon icon={"mastodon-fill"} link={"/r/mastodon"} />
+              <SocialIcon icon={"earth-fill"} link={"/r/planetmc"} />
+              <SocialIcon icon={"mic-fill"} link={"/r/anchor"} />
             </div>
           </div>
         </section>
       </div>
-      <p className="m-8 font-semibold">
-        (c) {new Date().getFullYear()}{" "}
-        <span className="text-text-secondary">Cubyx Network</span>・Quadratisch.
-        Praktisch. Gut.
-      </p>
+
+      <div>
+        <p className="mt-4 p-4 text-center font-semibold">
+          (c) {new Date().getFullYear()}{" "}
+          <span className="text-text-secondary">Cubyx Network</span>
+          ・Quadratisch. Praktisch. Gut.
+        </p>
+        <div className="mb-4 flex w-full items-center justify-center gap-4 text-text-third">
+          <Link href={"/impressum"}>
+            <a href={"/impressum"}>Impressum</a>
+          </Link>
+
+          <span>•</span>
+
+          <Link href={"/datenschutz"}>
+            <a href={"/datenschutz"}>Datenschutz</a>
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
--- src/components/footer/SocialIcon.tsx
+++ src/components/footer/SocialIcon.tsx
@@ -3,7 +3,7 @@ import Icon from "../icon/Icon";
 const SocialIcon = ({ icon, link }: { icon: string; link: string }) => {
   return (
     <a href={link} target="_blank" rel="noreferrer">
-      <Icon name={icon} className="ri-3x" />
+      <Icon name={icon} className="icon-2xl md:icon-3xl" />
     </a>
   );
 };
