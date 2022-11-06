27709a187a99e5ad1ccd395be4f1fd6aff8c669f
Sachsenspielt <sachsenspielt3@gmail.com>
2022-11-06 11:31:19 +0100

    feat: add concept for project page

---
+++ src/components/projects/ProjectCard.tsx
@@ -0,0 +1,69 @@
+import { Project, TeamMember } from "@prisma/client";
+import Image from "next/image";
+import Icon from "../icon/Icon";
+
+const ProjectCard = ({
+  project,
+}: {
+  project: Project & { authors: TeamMember[] };
+}) => {
+  const imageUrl = project.image_url || "/img/netzwerk.png";
+
+  return (
+    <div className="group flex h-96 w-48 flex-col overflow-hidden rounded-lg bg-background-secondary shadow-xl transition-all hover:w-96 dark:bg-background-secondary-dark">
+      <div className="flex aspect-square h-1/2 w-96 items-center justify-center">
+        <div className="h-full w-1/2 p-4">
+          <Image
+            src={imageUrl}
+            alt={project.name + " thumbnail"}
+            width={100}
+            height={100}
+            className="aspect-square h-full w-full rounded-lg"
+          />
+        </div>
+
+        <div className="h-full w-1/2 p-4">
+          <div className="flex h-1/2 gap-1">
+            <Icon name={"user-fill"} />
+            <ul className="flex flex-col overflow-y-auto">
+              {project.authors.length > 0
+                ? project.authors.map((author) => (
+                    <li key={author.id}>{author.username}</li>
+                  ))
+                : "Ich konnte keine Autoren finden..."}
+            </ul>
+          </div>
+
+          <div className="flex gap-1">
+            <Icon name={"calendar-line"} />
+            <span>{new Date(project.releaseDate).toLocaleDateString()}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="order-last h-1/2 w-full flex-wrap p-4">
+        <h2 className="text-2xl font-semibold">{project.name}</h2>
+        <p className="line-clamp-4">
+          {project.description} Lorem Ipsum dolor sit amet. Lorem Ipsum dolor
+          sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem
+          Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit
+          amet.Lorem Ipsum dolor sit amet.Lorem Ipsum dolor sit amet.
+        </p>
+
+        {project.download_link && (
+          <a
+            href={project.download_link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="invisible flex w-fit gap-1 group-hover:visible"
+          >
+            <Icon name={"download-line"} />
+            <span>Download</span>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/pages/projekte/index.tsx
+++ src/pages/projekte/index.tsx
@@ -1,10 +1,17 @@
-import type { NextPage } from "next";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import React from "react";
 import styles from "./Projects.module.css";
+import { prisma } from "../../lib/prisma";
+import superjson from "superjson";
+import { Project, TeamMember } from "@prisma/client";
+import ProjectCard from "../../components/projects/ProjectCard";
 
-const ProjectPage: NextPage = () => {
+const ProjectPage = ({
+  projects,
+}: {
+  projects: (Project & { authors: TeamMember[] })[];
+}) => {
   return (
     <>
       <Header
@@ -17,12 +24,49 @@ const ProjectPage: NextPage = () => {
           styles.hero + " " + "flex h-96 w-full items-center justify-center"
         }
       >
-        <h1 className="text-6xl font-extrabold">Unsere Projekte</h1>
+        <h1 className="text-center text-6xl font-extrabold">Unsere Projekte</h1>
       </div>
 
+      <main className="flex w-full flex-col items-center gap-8 p-8">
+        <h2 className="text-3xl font-bold">Aktuelle Projekte</h2>
+        <div className="flex w-full flex-wrap justify-center gap-4">
+          {projects
+            .filter((p) => new Date(p.releaseDate).getTime() > Date.now())
+            .map((project) => (
+              <ProjectCard project={project} key={project.id} />
+            ))}
+        </div>
+        <h2 className="text-3xl font-bold">Abgeschlossene Projekte</h2>
+        <div className="flex w-full justify-center gap-4">
+          {projects
+            .filter((p) => new Date(p.releaseDate).getTime() < Date.now())
+            .map((project) => (
+              <ProjectCard project={project} key={project.id} />
+            ))}
+        </div>
+      </main>
       <Footer />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  if (prisma) {
+    const projects = await prisma.project.findMany({
+      include: {
+        authors: true,
+      },
+    });
+
+    return {
+      props: superjson.serialize({ projects }).json,
+    };
+  }
+  return {
+    props: {
+      projects: [],
+    },
+  };
 };
 
 export default ProjectPage;
