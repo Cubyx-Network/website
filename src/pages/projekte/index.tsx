481bc5362b9fab8626c96804d2813c5b73cba584
Sachsenspielt <sachsenspielt3@gmail.com>
2022-11-06 09:49:29 +0100

    content: added hero to project page

--- src/pages/projekte/index.tsx
+++ src/pages/projekte/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import React from "react";
+import styles from "./Projects.module.css";
 
 const ProjectPage: NextPage = () => {
   return (
@@ -10,6 +11,14 @@ const ProjectPage: NextPage = () => {
         title={"Projekte"}
         description={"Hier findest du alle Projekte von Cubyx!"}
       />
+
+      <div
+        className={
+          styles.hero + " " + "flex h-96 w-full items-center justify-center"
+        }
+      >
+        <h1 className="text-6xl font-extrabold">Unsere Projekte</h1>
+      </div>
 
       <Footer />
     </>
