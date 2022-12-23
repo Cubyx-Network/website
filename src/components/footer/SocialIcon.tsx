41f8a35651a9795582d06e38ea2890e4aca20306
Sachsenspielt <sachsenspielt3@gmail.com>
2022-12-23 20:43:25 +0100

    fix: added me rel to sociallinks

--- src/components/footer/SocialIcon.tsx
+++ src/components/footer/SocialIcon.tsx
@@ -1,10 +1,11 @@
 import Icon from "../icon/Icon";
+import Link from "next/link";
 
 const SocialIcon = ({ icon, link }: { icon: string; link: string }) => {
   return (
-    <a href={link} target="_blank" rel="noreferrer">
+    <Link href={link} target="_blank" rel="noreferrer me">
       <Icon name={icon} className="icon-2xl md:icon-3xl" />
-    </a>
+    </Link>
   );
 };
 
