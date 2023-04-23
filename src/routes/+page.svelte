dcdba9013f81dfbe33576ef46fdfad3fab269672
Sachsenspielt <sachsenspielt3@gmail.com>
2023-04-23 16:22:59 +0200

    feat: first responsiveness optimization; header wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-    import Hero from '../components/Hero/Hero.svelte';
+	import Hero from '../components/Hero/Hero.svelte';
 </script>
 
 <Hero img='/img/home/hero.png'>
-	<div class='text-center leading-[4rem] dshadow'>
+	<div class='text-center leading-[2rem] md:leading-[3rem] lg:leading-[4rem] dshadow'>
 		<h1 class='tracking-[.3em] '>CUBYX NETWORK</h1>
 		<h3>Quadratisch<span class='text-accent'>.</span> Praktisch<span class='text-accent'>.</span>
 			Gut<span class='text-accent'>.</span></h3>
