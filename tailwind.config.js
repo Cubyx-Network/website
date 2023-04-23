4f50c6db7eedb3607ac02e538492b23f4517a543
Sachsenspielt <sachsenspielt3@gmail.com>
2023-04-23 12:49:37 +0200

    theme: added theme variables and font

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				background: '#284744',
+				text: '#ffffff',
+				accent: '#23AA08',
+				grayedOut_dark: '#797979',
+				grayedOut_light: '#969696'
+			}
+		},
+		fontFamily: {
+			text: ['Kanit', 'sans-serif'],
+			heading: ['Montserrat', 'sans-serif']
+		}
+	},
+	plugins: []
+};
