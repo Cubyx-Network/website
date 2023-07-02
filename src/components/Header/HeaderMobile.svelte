2195b178c1943867f88e2d66cffa2a1d3731964d
Sachsenspielt <sachsenspielt3@gmail.com>
2023-07-02 19:20:57 +0200

    fix: fixed lint

--- src/components/Header/HeaderMobile.svelte
+++ src/components/Header/HeaderMobile.svelte
@@ -13,16 +13,16 @@
 </script>
 
 <nav
-	class="fixed z-10 flex flex h-24 w-full items-center justify-center gap-5 bg-background_darker p-1 lg:hidden"
+	class="fixed z-10 flex h-24 w-full items-center justify-center gap-5 bg-background_darker p-1 lg:hidden"
 >
-	<div on:click={handleClick} class="z-10">
+	<button on:click={handleClick} class="z-10">
 		<HeaderLogo rotateDown={isOpen} />
-	</div>
+	</button>
 
 	{#if isOpen}
 		<div
 			transition:fly={{ y: -200, duration: 300 }}
-			class="absolute top-0 z-0 flex h-screen h-screen w-full flex-col items-center justify-center gap-5 bg-background_darker"
+			class="absolute top-0 z-0 flex h-screen w-full flex-col items-center justify-center gap-5 bg-background_darker"
 		>
 			{#each links as link}
 				<HeaderLink link={link.link} text={link.text} onClick={handleClick} />
