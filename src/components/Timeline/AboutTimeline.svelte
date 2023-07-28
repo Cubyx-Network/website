8c5cfc641dbf4286ede8c2d7f177c06b88a9a14b
Sachsenspielt <sachsenspielt3@gmail.com>
2023-07-28 18:33:03 +0200

    fix: changed some text in timeline

--- src/components/Timeline/AboutTimeline.svelte
+++ src/components/Timeline/AboutTimeline.svelte
@@ -4,37 +4,31 @@
   let items = [
     {
       title: "Cubyx wird als reines Bauteam eröffnet",
-      description: "",
       date: "Januar 2020",
       position: "left",
     },
     {
       title: "Netzwerk-Aufbau",
-      description: "",
       date: "Oktober 2020",
       position: "right",
     },
     {
       title: 'Cubyx startet sein erstes Survival Projekt "Tecta"',
-      description: "",
       date: "August 2021",
       position: "left",
     },
     {
-      title: "Cubyx schließt seine Forten der Öffentlichkeit gegenüber",
-      description: "",
+      title: "Cubyx schließt seine Pforten der Öffentlichkeit gegenüber",
       date: "Oktober 2021",
       position: "right",
     },
     {
       title: "Cubyx wird Netzwerk Team",
-      description: "",
       date: "Juni 2022",
       position: "left",
     },
     {
       title: "Cubyx veröffentlicht das Netzwerk neu!",
-      description: "",
       date: "Dezember 2022",
       position: "right",
     },
