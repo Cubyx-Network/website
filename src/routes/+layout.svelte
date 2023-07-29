46016901fd7f8fc46eb4600805cc446baaf53c5d
SACHSENSPIELT <59836848+SachsenspieltCoding@users.noreply.github.com>
2023-07-29 10:25:55 +0200

    change: Verlinkung zum Bewerbungsformular hinzugefügt (#60)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script>
-	import '../app.css';
-	import Header from '../components/Header/Header.svelte';
-	import Footer from '../components/Footer/Footer.svelte';
-	import HeaderDummy from '../components/Header/HeaderDummy.svelte';
+	import "../app.css";
+	import Header from "../components/Header/Header.svelte";
+	import Footer from "../components/Footer/Footer.svelte";
+	import HeaderDummy from "../components/Header/HeaderDummy.svelte";
 
 	let headerConfig = [
 		{ text: 'Home', link: '/' },
 		{ text: 'Das Team', link: '/team' },
 		{ text: 'Neuigkeiten', link: '/news' },
 		{ text: 'Partner', link: '/partner' },
-		{ text: 'Bewerben', link: '/bewerben' },
+		{ text: 'Bewerben', link: 'https://cubyx.eu/bewerben' },
 		{ text: 'Discord', link: 'https://cubyx.eu/discord' }
 	];
 </script>
