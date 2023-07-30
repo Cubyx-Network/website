fc93ac7fb9524f3bea77132c4aeccbbac63ad53c
Sachsenspielt <sachsenspielt3@gmail.com>
2023-07-30 23:20:01 +0200

    style: fixed ugly styling

--- src/components/Timeline/TimelineItem.svelte
+++ src/components/Timeline/TimelineItem.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-	import type { TimelineItemType } from './Timeline.svelte';
-	import { twMerge } from 'tailwind-merge';
+	import type { TimelineItemType } from "./Timeline.svelte";
+	import { twMerge } from "tailwind-merge";
 
 	export let item: TimelineItemType;
 </script>
 
 <div
-	class={twMerge(
-		'w-[45%]',
-		item.position === 'left' ? 'mr-8 -translate-x-1/2 text-right' : 'ml-8 translate-x-1/2'
-	)}
+	class="w-full flex flex-col items-center"
 >
-	<div class="absolute h-full w-full">
-		<div
-			class={twMerge(
-				'absolute top-[.5rem] h-5 w-5 rounded-full bg-accent',
+	<div
+		class={twMerge(
+				'mt-[.5rem] h-5 w-5 rounded-full bg-accent',
 				item.position === 'left' ? '-right-[1.6rem]' : '-left-[1.6rem]'
 			)}
-		/>
+	/>
+	<div class={twMerge('-translate-y-[1.9rem] max-w-[40%]', item.position === "left" ? "text-right -translate-x-1/2 mr-8" : "ml-8 translate-x-1/2")}>
+		<p class="text-grayedOut_light">{item.date}</p>
+		<p class="whitespace-normal break-words font-medium">{item.title}</p>
+		{#if item.description}<p>{item.description}</p>{/if}
 	</div>
-	<p class="text-grayedOut_light">{item.date}</p>
-	<p class="whitespace-normal break-words font-medium">{item.title}</p>
-	{#if item.description}<p>{item.description}</p>{/if}
 </div>
