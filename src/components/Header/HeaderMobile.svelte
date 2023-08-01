ea65dfcb72b1b5182cccd39b45bba35cb906e0a2
Sachsenspielt <sachsenspielt3@gmail.com>
2023-08-01 19:40:54 +0200

    fix: headmobile not adapted to daisyUI

--- src/components/Header/HeaderMobile.svelte
+++ src/components/Header/HeaderMobile.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import HeaderLogo from './HeaderLogo.svelte';
-	import { fly } from 'svelte/transition';
-	import HeaderLink from './HeaderLink.svelte';
+	import HeaderLogo from "./HeaderLogo.svelte";
+	import { fly } from "svelte/transition";
+	import HeaderLink from "./HeaderLink.svelte";
 
 	export let links: { link: string; text: string }[] = [];
 
@@ -13,7 +13,7 @@
 </script>
 
 <nav
-	class="fixed z-10 flex h-24 w-full items-center justify-center gap-5 bg-background_darker p-1 lg:hidden"
+	class="sticky top-0 z-10 flex h-24 w-full items-center justify-center gap-5 bg-secondary p-1 lg:hidden"
 >
 	<button on:click={handleClick} class="z-10">
 		<HeaderLogo rotateDown={isOpen} />
@@ -22,7 +22,7 @@
 	{#if isOpen}
 		<div
 			transition:fly={{ y: -200, duration: 300 }}
-			class="absolute top-0 z-0 flex h-screen w-full flex-col items-center justify-center gap-5 bg-background_darker"
+			class="absolute top-0 z-0 flex h-screen w-full flex-col items-center justify-center gap-5 bg-secondary"
 		>
 			{#each links as link}
 				<HeaderLink link={link.link} text={link.text} onClick={handleClick} />
