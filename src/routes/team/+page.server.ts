d163c58a5a9103bdb464784e9b936e256c237980
Sachsenspielt <sachsenspielt3@gmail.com>
2023-08-01 21:03:40 +0200

    feat: completed team page

--- src/routes/team/+page.server.ts
+++ src/routes/team/+page.server.ts
@@ -2,18 +2,16 @@ import type { PageServerLoad } from './$types';
 import prisma from '$lib/server/prisma';
 
 export const load = (async () => {
-	const test = await prisma.teamMember.findMany({
-		include: {
-			subunits: true,
-			status: true
-		}
-	});
-
 	return {
 		members: await prisma.teamMember.findMany({
 			include: {
 				subunits: true,
 				status: true
+			}
+		}),
+		statuses: await prisma.teamMemberStatus.findMany({
+			include: {
+				members: true
 			}
 		})
 	};
