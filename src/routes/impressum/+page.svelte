25d208e8b72f9dd846559267c43a7795ebb47d8f
Sachsenspielt <sachsenspielt3@gmail.com>
2023-08-02 11:01:39 +0200

    content: Impressum und Datenschutzerklärung hinzugefügt (#61) (#72)

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+	import { generateTitle } from "$lib/titleGenerator";
+	import HeroWithTitleAndSlogan from "../../components/Common/HeroWithTitleAndSlogan.svelte";
+</script>
+
+<svelte:head>
+	<title>{generateTitle('Impressum')}</title>
+</svelte:head>
+
+<HeroWithTitleAndSlogan img="/img/legal/hero.jpg">Impressum</HeroWithTitleAndSlogan>
+
+<div class="space-y-8 p-4 lg:p-16">
+	<h3>Angaben gemäß § 5 TMG</h3>
+	<div class="flex gap-16">
+		<p>
+			Rudolf Müller<br />
+			Siedlung Roßweiner Straße 20<br />
+			Untermieter vom Baumhaus<br />
+			04720 Döbeln
+		</p>
+
+		<p>
+			Maurice Reyher<br />
+			Ernstweg 4<br />
+			98716 Elgersburg
+		</p>
+	</div>
+
+	<p class="font-bold">
+		Hinweis: Der Missbrauch von jeglichen Kontakt und personenbezogenen Daten wird zur Anzeige
+		gebracht!
+	</p>
+
+	<p>
+		<strong>Vertreten durch: </strong><br />
+		Rudolf Müller<br />
+	</p>
+	<p>
+		<strong>Allgemeiner Kontakt:</strong> <br />
+		E-Mail: <a href="mailto:info@cubyx.eu">info@cubyx.eu</a>
+	</p>
+	<h2>Haftungsausschluss</h2>
+	<p>
+		<strong>Haftung für Inhalte</strong><br />
+		Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit
+		und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir
+		gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
+		Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte
+		fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
+		hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
+		Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt
+		der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen
+		werden wir diese Inhalte umgehend entfernen.<br /><br /><strong>Haftung für Links</strong><br />
+		Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss
+		haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte
+		der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.
+		Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+		Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
+		Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht
+		zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br
+		/><br /><strong>Urheberrecht</strong><br />
+		Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
+		Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb
+		der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+		Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+		Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte
+		Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem
+		auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
+		Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br /><br
+		/><strong>Datenschutz</strong><br />
+		Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit
+		auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben
+		werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre
+		ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br />
+		Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail)
+		Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist
+		nicht möglich. <br />
+		Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung
+		von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich
+		widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der
+		unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.<br />
+	</p>
+	<p>
+		Impressum vom <a href="https://www.impressum-generator.de">Impressum Generator</a> der
+		<a href="https://www.kanzlei-hasselbach.de/standorte/frankfurt/" rel="nofollow"
+			>Kanzlei Hasselbach, Frankfurt</a
+		>
+	</p>
+	<p />
+</div>
