371869bb4f05d722db9a2112dd21f08ada5de967
Sachsenspielt <sachsenspielt3@gmail.com>
2023-11-06 17:47:43 +0100

    chore: Entfernung obsoleter Components

--- src/components/Timeline/TimelineIcon.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { faLink } from '@fortawesome/free-solid-svg-icons/faLink';
-	import { Icon } from 'svelte-awesome';
-	import type { TimelineItemType } from './Timeline.svelte';
-
-	export let item: TimelineItemType;
-</script>
-
-<a href={item.href} target="_blank" class="flex items-center"> </a>
