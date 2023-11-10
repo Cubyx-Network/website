59c2c132c4968a666c2094f8d4e09f68562eb67f
Sachsenspielt <sachsenspielt3@gmail.com>
2023-11-10 19:21:20 +0100

    ci: Netlify für Branch & PR Deployments hinzugefügt

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: process.env.NETLIFY
+			? netlifyAdapter({
+					edge: false
+			  })
+			: adapter()
 	}
 };
 
