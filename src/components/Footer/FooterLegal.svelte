101d3cd55868fffb9095c3a01d3aa127b5677bc1
Finn | Sachsenspielt <59836848+SachsenspieltCoding@users.noreply.github.com>
2023-11-10 21:31:31 +0100

    feat: Weiterleitungshinweise im Footer hinzugefügt (#182)

--- src/components/Footer/FooterLegal.svelte
+++ src/components/Footer/FooterLegal.svelte
@@ -1,9 +1,19 @@
+<script>
+	import { faChain } from '@fortawesome/free-solid-svg-icons';
+
+	import { Icon } from 'svelte-awesome';
+</script>
+
 <div class="flex flex-wrap items-center justify-center gap-x-2 self-center text-neutral md:gap-4">
 	<a href="/impressum">Impressum</a>
 	<p class="text-neutral-focus">•</p>
 	<a href="/datenschutz">Datenschutz</a>
 	<p class="text-neutral-focus">•</p>
-	<a href="https://cubyx.eu/status" target="_blank">Statuspage</a>
+	<a href="https://cubyx.eu/status" target="_blank" class="flex items-center gap-1">
+		Statuspage <Icon data={faChain} />
+	</a>
 	<p class="text-neutral-focus">•</p>
-	<a href="https://bio.link/cubyx">Kontakt</a>
+	<a href="https://bio.link/cubyx" class="flex items-center gap-1">
+		Kontakt <Icon data={faChain} />
+	</a>
 </div>
