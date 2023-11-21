5ce69d38293b8056c78d78f97291272f05cbaee0
Sachsenspielt <sachsenspielt3@gmail.com>
2023-11-21 22:02:24 +0100

    feat: Ladebalken für Seitenwechsel hinzugefügt

---
+++ src/components/Common/PageLoadingBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+
+	const progress = tweened(0, {
+		delay: 250,
+		duration: 5000,
+		easing: cubicOut
+	});
+
+	onMount(() => {
+		progress.set(0.95);
+	});
+
+	onDestroy(() => {});
+</script>
+
+<div class="fixed left-0 top-0 z-50 h-1 w-screen" in:fade={{ delay: 250 }} out:fade>
+	<div class="h-full w-[var(--width)] bg-primary" style={`--width: ${$progress * 100}%`} />
+</div>
