acba58f4080c6f340fe8eea62afdcb3f990d7054
Sachsenspielt <sachsenspielt3@gmail.com>
2023-11-25 19:13:39 +0100

    chore: Imports im FooterSocialIcon verbessert

--- src/components/Footer/FooterSocialIcon.svelte
+++ src/components/Footer/FooterSocialIcon.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-	import type { IconDefinition as BrandIconDefinition } from "@fortawesome/free-brands-svg-icons";
-	import type { IconDefinition as SolidIconDefinition } from "@fortawesome/free-solid-svg-icons";
-	import type { IconDefinition as RegularIconDefinition } from "@fortawesome/free-regular-svg-icons";
-	import { Icon } from "svelte-awesome";
+	import { Icon } from 'svelte-awesome';
+	import type { FaIconDefinition } from 'svelte-awesome/components/Icon.svelte';
 
-	export let icon: BrandIconDefinition | SolidIconDefinition | RegularIconDefinition;
+	export let icon: FaIconDefinition;
 	export let href: string;
 </script>
 
 <a {href} target="_blank">
-	<Icon data={icon} class="h-6 w-auto text-base-content text-[2em] md:h-8 lg:h-12" />
+	<Icon data={icon} class="h-6 w-auto text-[2em] text-base-content md:h-8 lg:h-12" />
 </a>
