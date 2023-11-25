0fd6056d1fae02e73f3e612001ec483806d3318d
Sachsenspielt <sachsenspielt3@gmail.com>
2023-11-25 19:43:05 +0100

    neuer desingvorschlag

--- src/components/Team/TeamMemberCard.svelte
+++ src/components/Team/TeamMemberCard.svelte
@@ -8,12 +8,12 @@
 	export let labels: TeamMemberLabel[];
 </script>
 
-<div class="card rounded-box bg-secondary">
-	<figure>
+<div class="card rounded-box bg-secondary sm:card-side">
+	<figure class="w-full sm:w-1/3">
 		<img
 			src="/api/teamMember/avatar/{teamMember.id}"
 			alt="Avatar von {teamMember.name}"
-			class="w-full"
+			class="h-full w-full object-cover"
 		/>
 	</figure>
 	<div class="card-body">
@@ -22,7 +22,7 @@
 				<TeamMemberLabelComponent {label} />
 			{/each}
 		</div>
-		<h2 class="card-title w-full break-all">{teamMember.name}</h2>
+		<h2 class="card-title w-full">{teamMember.name}</h2>
 		<span class="flex items-center gap-2 text-neutral">
 			<Icon data={faDiscord} />
 			<p class="w-full text-sm">{teamMember.discord}</p>
